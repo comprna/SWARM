b7095957a9dca0b541313e75950c02f1cbaee7bd
Stefan Prodic <97370897+stefanprodic@users.noreply.github.com>
2025-10-31 10:17:57 +1100

    Update argument flags for input and output files

--- SWARM_scripts/SWARM_diff.py
+++ SWARM_scripts/SWARM_diff.py
@@ -214,9 +214,9 @@ def apply_GLM(df):
 if __name__ == "__main__":
     # parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_file',
+    parser.add_argument('-i','--data_file',
                         help='tab-separated text files with first column M2_file_path, second column RepName, third column Condition')
-    parser.add_argument('--output_file', help='Output table.')
+    parser.add_argument('-o','--output_file', help='Output table.')
     parser.add_argument('-p', '--M2_threshold', type=float, default=0,
                         help='Compare only sites with at least one sample modified with site-probability more than p. Default =0')
     parser.add_argument('-s', '--Stoich_threshold', type=float, default=0,
