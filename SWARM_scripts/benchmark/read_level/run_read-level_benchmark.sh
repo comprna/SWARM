ab2fc25fd924d2eb2cd9b1fd0b3a75f31e159bd1
Stefan Prodic <97370897+stefanprodic@users.noreply.github.com>
2026-09-22 16:11:39 +1000

    Update read-level benchmark script

--- SWARM_scripts/benchmark/read_level/run_read-level_benchmark.sh
+++ SWARM_scripts/benchmark/read_level/run_read-level_benchmark.sh
@@ -1,17 +1,17 @@
 module load singularity
 
-SIF=../../tensorflow_24.01-tf2-py3-pysam.sif
+SIF=tensorflow_24.01-tf2-py3-pysam.sif
 FASTA=IVT-m_data/I-M_combined.fasta
-SCRIPT=../../SWARM_scripts/SWARM_read_level.py ;
+SCRIPT=../../SWARM_read_level.py ;
+
+mkdir outputs
 
 for target_mod in "m6A" "m5C" "pU"; do
         for sample_mod in "m6A" "m5C" 'pU' "NM"; do
                 BLOW5=IVT-m_data/blow5/IVT-m1-m2.$sample_mod.blow5 ;
                 SAM=IVT-m_data/events/IVT-m1-m2.$sample_mod.events.sam ;
-                OUT=IVT-m1-m2.sample-${sample_mod}.taret-${target_mod}.pred.tsv ;
+                OUT=outputs/IVT-m1-m2.sample-${sample_mod}.target-${target_mod} ;
                 singularity exec --nv $SIF python3 $SCRIPT -m $target_mod --sam $SAM --fasta $FASTA --raw $BLOW5 -o $OUT ;
         done ;
 
 done
-
-singularity exec --nv $SIF python3 plot_read_level_benchmark.py
